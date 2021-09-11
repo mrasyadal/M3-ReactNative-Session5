4df3c7835c5c1bf31b196a6ef427e1716c0adda6
mrasyadal <m.rasyad.a.l@gmail.com>
2021-09-11 23:01:29 +0700

    Working version of the app from Chapter 1 and 2

--- App.js
+++ App.js
@@ -1,12 +1,24 @@
 import React from 'react';
 import {Text, View} from 'react-native';
 import {NavigationContainer} from '@react-navigation/native';
-import MainStack from './src/navigators/MainStack';
+//import MainStack from './src/navigators/MainStack';
+import {createNativeStackNavigator} from '@react-navigation/native-stack';
+import Home from './src/screens/Home';
+import UserProfile from './src/screens/UserProfile';
+import Comments from './src/screens/Comments';
+
+const Stack = createNativeStackNavigator();
 
 const App = () => {
   return (
     <NavigationContainer>
-      <MainStack />
+      <Stack.Navigator initialRouteName="Home">
+        {/* Stack.Navigator memiliki initial route name 'Home' yang mengeset `Home` jadi menu utama*/}
+        {/* Stack.Navigator berisi Home, UserProfile, dan Comments */}
+        <Stack.Screen name="Home" component={Home} />
+        <Stack.Screen name="UserProfile" component={UserProfile} />
+        <Stack.Screen name="Comments" component={Comments} />
+      </Stack.Navigator>
     </NavigationContainer>
   );
 };
